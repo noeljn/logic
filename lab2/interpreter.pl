02d973328b0b999b7a554ab8c7f1e619e56dcdf6
noeljn <noel.johansson00@gmail.com>
2020-11-15 18:18:23 +0100

    Added some stuff

--- lab2/interpreter.pl
+++ lab2/interpreter.pl
@@ -6,14 +6,60 @@ verify(InputFileName) :- see(InputFileName),
 
 valid_proof(Prems, Goal, Proof) :- verifyLastLine(Goal, Proof), verifyProof(Proof, Prems, Proof).
 
-verifyLastLine(Goal, Proof) :- 
+%
+verifyLastLine(Goal, Proof) :- last(Proof,LastLine), checkLastLine(LastLine, Goal).
+
+%Checks if the term of the last line is equal to the Goal
+checkLastLine([_, Term, _], Goal) :- Term == Goal.
+
+%
+verifyProof([], _, _).
+verifyProof([H|T], Prems, Proof) :- verifyLine(H, Prems, Proof), verifyProof(T, Prems, Proof). 
+
+verifyLine([_, Term, premise], Prems, _) :- member(Term, Prems).
+
+verifyLine([LineNum, Term, copy(X)], _, Proof) :- X < LineNum, findTerm(X, Proof, Term).
+
+verifyLine([LineNum, Term, andint(X,Y)], _, Proof) :- X < LineNum, Y < LineNum, findTerm(X, Proof, TermX), findTerm(Y, Proof, TermY), Term == and(TermX, TermY).
+
+getFirstOfList([H|T], H).
+getSecondOfList([_,B|T], B).
+getThirdOfList([_,_,C|T], C).
+
+findTerm(LineNum,[], Term) :- false.
+findTerm(LineNum, [H|T], Term) :- getFirstOfList(H, LineNum), getSecondOfList(H, Term).
+findTerm(LineNum, [H|T], Term) :- findTerm(LineNum, T, Term).
+
+
+premise(X,Y) :- false.
+%verifyLine([[LineNum, Term, assumption]|T], Prems, Proof) :-
+
+
+
+
 /*
-verifyLastLine
 
-verifyLine
 
-verifyProof
-copY(X)
+% En lista av premisser (v¨anstra delen av sekventen.)
+[neg(neg(imp(p, neg(p))))].
+
+% M˚alet (h¨ogra delen av sekventen).
+neg(p).
+
+% Beviset
+
+[
+    [1, neg(neg(imp(p,neg(p)))), premise ],
+    [2, imp(p, neg(p)), negnegel(1)],
+    [
+        [3, p, assumption ],
+        [4, neg(p), impel(3,2) ],
+        [5, cont, negel(3,4) ]
+    ],
+    [6, neg(p), negint(3,5)]
+].
+
+copy(X)
 
 premise(X,Y)
 
