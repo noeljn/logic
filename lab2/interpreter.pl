3b15db8234caebecd6418866a3f4da4edf779547
noeljn <noel.johansson00@gmail.com>
2020-11-16 20:10:11 +0100

    Updated

--- lab2/interpreter.pl
+++ lab2/interpreter.pl
@@ -46,9 +46,30 @@ findTerm(U, Proof, Term),
 findTerm(V, Proof, TermY), 
 findTerm(W, Proof, Term), lineVerifyPrint(LineNum).  
 
-verifyLine([LineNum, imp(TermX,TermY), impint(X,Y)], _, Proof) :- X < LineNum, Y < LineNum, findTerm(X, Proof, TermX), findTerm(Y, Proof, TermY), compareLevel(LineNum, Proof, impint(X,Y)), lineVerifyPrint(LineNum).
+verifyLine([LineNum, imp(TermX,TermY), impint(X,Y)], _, Proof) :- X < LineNum, Y < LineNum, findTerm(X, Proof, TermX), findTerm(Y, Proof, TermY), lineVerifyPrint(LineNum).
 
-verifyLine([LineNum, Term, impel(X,Y)], _, Proof) :- X < LineNum, Y < LineNum, findTerm(Y, Proof, imp(TermX, Term)), findTerm(X, Proof, TermX), compareLevel(LineNum, Proof, impel(X,Y)), lineVerifyPrint(LineNum).
+verifyLine([LineNum, Term, impel(X,Y)], _, Proof) :- X < LineNum, Y < LineNum, findTerm(Y, Proof, imp(TermX, Term)), findTerm(X, Proof, TermX), lineVerifyPrint(LineNum).
+
+verifyLine([LineNum, Term, negint(X,Y)], _, Proof) :- X < LineNum, Y < LineNum, findTerm(X, Proof, TermX), findTerm(Y, Proof, cont), neg(TermX) == Term, lineVerifyPrint(LineNum).
+verifyLine([LineNum, Term, negel(X,Y)], _, Proof) :- X < LineNum, Y < LineNum, findTerm(X, Proof, TermX), findTerm(Y, Proof, TermY), neg(TermX) == TermY, Term == cont, lineVerifyPrint(LineNum).
+
+verifyLine([LineNum, _, contel(X)], _, Proof) :- X < LineNum, findTerm(X, Proof, cont), lineVerifyPrint(LineNum).
+%contel(X)
+
+%negnegint(X)
+%negnegel(X)
+verifyLine([LineNum, Term, negnegint(X)], _, Proof) :- X < LineNum, findTerm(X, Proof, TermX), arg(1, Term, InTerm), arg(1, InTerm, TermX), lineVerifyPrint(LineNum). 
+verifyLine([LineNum, Term, negnegel(X)], _, Proof) :- X < LineNum, findTerm(X, Proof, neg(neg(Term))), lineVerifyPrint(LineNum).
+
+%mt(X,Y)
+verifyLine([LineNum, Term, mt(X,Y)], _, Proof) :- X < LineNum , Y < LineNum, arg(1, Term, InTerm), findTerm(X, Proof, TermX), arg(1, TermX, InTerm), arg(2, TermX, InTermX ), findTerm(Y, Proof, neg(InTermX)), lineVerifyPrint(LineNum).
+
+%pbc(X,Y)
+verifyLine([LineNum, Term, pbc(X,Y)], _, Proof) :- X < LineNum, Y < LineNum, findTerm(X, Proof, neg(Term)), findTerm(Y, Proof, cont), lineVerifyPrint(LineNum).
+
+%lem
+verifyLine([LineNum, or(neg(P), P), lem], _, Proof) :- lineVerifyPrint(LineNum).
+verifyLine([LineNum, or(P, neg(P)), lem], _, Proof) :- lineVerifyPrint(LineNum).
 
 getFirstOfList([H|T], H).
 getSecondOfList([_,B|T], B).
@@ -64,10 +85,17 @@ findTerm(LineNum, [Line|T], Term) :- findTerm(LineNum, T, Term).
 lineVerifyPrint(LineNum) :- write("Line "),write(LineNum),write(" fullfilled"),write("\n").
 levelPrint(LineNum) :- write("Level "),write(LineNum),write("\n").
 
+%Finds the level of a line
 boxLevel(LineNum,[], BoxLevel, ResBoxLevel) :- false.
 boxLevel(LineNum, [Line|T], BoxLevel, ResBoxLevel) :- getFirstOfList(Line, LineNum), ResBoxLevel = BoxLevel.
 boxLevel(LineNum, [Line|T], BoxLevel, ResBoxLevel) :- getFirstOfList(Line, H), isList(H), NewLevel is BoxLevel + 1, boxLevel(LineNum, Line, NewLevel, ResBoxLevel).
 boxLevel(LineNum, [Line|T], BoxLevel, ResBoxLevel) :- boxLevel(LineNum, T, BoxLevel, ResBoxLevel).
+
+%Dosent work :(
+boxNumber(LineNum,[], BoxLevel, ResBoxLevel, InBox) :- false.
+boxNumber(LineNum, [Line|T], BoxNum, ResBoxNum, InBox) :- getFirstOfList(Line, LineNum), ResBoxNum = BoxNum.
+boxNumber(LineNum, [Line|T], BoxNum, ResBoxNum, InBox) :- getFirstOfList(Line, H), isList(H), (InBox == 0 -> InBox is 1, NewBoxNum is BoxNum + 1), boxNumber(LineNum, Line, NewBoxNum, ResBoxNum, InBox), InBox is 0.
+boxNumber(LineNum, [Line|T], BoxNum, ResBoxNum, InBox) :- boxNumber(LineNum, T, BoxNum, ResBoxNum, InBox).
 
 %Checks if argument is list 
 isList([_|_]).
@@ -79,51 +107,6 @@ boxLevel(LineNum, Proof, 0, Level), boxLevel(Value, Proof, 0, ValueLevel), boxLe
 Level >= ValueLevel, Level >= SecValueLevel.
 
 /*
-
-    
-% En lista av premisser (v¨anstra delen av sekventen.)
-[neg(neg(imp(p, neg(p))))].
-
-% M˚alet (h¨ogra delen av sekventen).
-neg(p).
-
-% Beviset
-
-[
-    [1, neg(neg(imp(p,neg(p)))), premise ],
-    [2, imp(p, neg(p)), negnegel(1)],
-    [
-        [3, p, assumption ],
-        [4, neg(p), impel(3,2) ],
-        [5, cont, negel(3,4) ]
-    ],
-    [6, neg(p), negint(3,5)]
-].
-
-copy(X)
-
-premise(X,Y)
-
-assumption(X,Y)
-
-andint(X,Y)
-andel1(X)
-andel2(X)
-
-orint1(X)
-orint2(X)
-
-orel(X,Y,U,V,W)
-
-impint(X,Y)
-
-impel(X,Y)
-
-negint(X,Y)
-
-negel(X,Y)
-
-contel(X)
 
 negnegint(X)
 negnegel(X)
