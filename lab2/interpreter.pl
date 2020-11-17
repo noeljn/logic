e13a1715e76d9bd8c247f8b9589e80aeee54644b
noeljn <noel.johansson00@gmail.com>
2020-11-17 19:46:48 +0100

    Final Form

--- lab2/interpreter.pl
+++ lab2/interpreter.pl
@@ -26,7 +26,7 @@ verifyLine([[LineNum, Term, assumption]|T], Prems, Proof) :- verifyProof(T, Prem
 verifyLine([LineNum, Term, copy(X)], _, Proof) :- X < LineNum, findTerm(X, Proof, Term), compareLevel(LineNum, Proof, copy(X)), compareBoxNumber(LineNum, Proof, copy(X)), ! , lineVerifyPrint(LineNum).
 
 %andint
-verifyLine([LineNum, Term, andint(X,Y)], _, Proof) :- X < LineNum, Y < LineNum, findTerm(X, Proof, TermX), findTerm(Y, Proof, TermY), Term == and(TermX, TermY), lineVerifyPrint(LineNum).
+verifyLine([LineNum, Term, andint(X,Y)], _, Proof) :- X < LineNum, Y < LineNum, findTerm(X, Proof, TermX), findTerm(Y, Proof, TermY), Term == and(TermX, TermY), compareBoxNumber(LineNum, Proof, andint(X,Y)), lineVerifyPrint(LineNum).
 
 %andel
 verifyLine([LineNum, Term, andel1(X)], _, Proof) :- X < LineNum, findTerm(X, Proof, and(Term,_)), compareLevel(LineNum, Proof, andel1(X)), compareBoxNumber(LineNum, Proof, andel1(X)), lineVerifyPrint(LineNum).
@@ -42,20 +42,22 @@ findTerm(X, Proof, or(TermX, TermY)),
 findTerm(Y, Proof, TermX),
 findTerm(U, Proof, Term), 
 findTerm(V, Proof, TermY), 
-findTerm(W, Proof, Term), lineVerifyPrint(LineNum).  
+findTerm(W, Proof, Term),
+compareLevel(LineNum, Proof, orel(X,Y,U,V,W)), compareBoxNumber(LineNum, Proof, orel(X,Y,U,V,W)),
+!,lineVerifyPrint(LineNum).  
 
-verifyLine([LineNum, imp(TermX,TermY), impint(X,Y)], _, Proof) :- X < LineNum, Y < LineNum, findTerm(X, Proof, TermX), findTerm(Y, Proof, TermY), compareLevel(LineNum, Proof, impint(X,Y)), compareBoxes(LineNum, Proof, impel(X,Y)), ! , lineVerifyPrint(LineNum).
+verifyLine([LineNum, imp(TermX,TermY), impint(X,Y)], _, Proof) :- X < LineNum, Y < LineNum, findTerm(X, Proof, TermX), findTerm(Y, Proof, TermY), compareLevel(LineNum, Proof, impint(X,Y)), compareBoxNumber(LineNum, Proof, impint(X,Y)), ! , lineVerifyPrint(LineNum).
 
-verifyLine([LineNum, Term, impel(X,Y)], _, Proof) :- X < LineNum, Y < LineNum, findTerm(Y, Proof, imp(TermX, Term)), findTerm(X, Proof, TermX), compareLevel(LineNum, Proof, impel(X,Y)), lineVerifyPrint(LineNum).
+verifyLine([LineNum, Term, impel(X,Y)], _, Proof) :- X < LineNum, Y < LineNum, findTerm(Y, Proof, imp(TermX, Term)), findTerm(X, Proof, TermX), compareLevel(LineNum, Proof, impel(X,Y)), compareBoxNumber(LineNum, Proof, impel(X,Y)), lineVerifyPrint(LineNum).
 
-verifyLine([LineNum, Term, negint(X,Y)], _, Proof) :- X < LineNum, Y < LineNum, findTerm(X, Proof, TermX), findTerm(Y, Proof, cont), neg(TermX) == Term, compareLevel(LineNum, Proof, negint(X,Y)), compareBoxes(LineNum, Proof, impel(X,Y)), lineVerifyPrint(LineNum).
+verifyLine([LineNum, Term, negint(X,Y)], _, Proof) :- X < LineNum, Y < LineNum, findTerm(X, Proof, TermX), findTerm(Y, Proof, cont), neg(TermX) == Term, compareLevel(LineNum, Proof, negint(X,Y)), lineVerifyPrint(LineNum).
 verifyLine([LineNum, Term, negel(X,Y)], _, Proof) :- X < LineNum, Y < LineNum, findTerm(X, Proof, TermX), findTerm(Y, Proof, TermY), neg(TermX) == TermY, compareLevel(LineNum, Proof, negel(X,Y)), Term == cont, ! , lineVerifyPrint(LineNum) .
 
-verifyLine([LineNum, _, contel(X)], _, Proof) :- X < LineNum, findTerm(X, Proof, cont), compareLevel(LineNum, Proof, contel(X)), compareBoxNumber(LineNum, Proof, compareBoxNumber(X)), lineVerifyPrint(LineNum).
-%contel(X)
+%contel
+verifyLine([LineNum, _, contel(X)], _, Proof) :- X < LineNum, findTerm(X, Proof, cont), compareLevel(LineNum, Proof, contel(X)), compareBoxNumber(LineNum, Proof, contel(X)), lineVerifyPrint(LineNum).
 
-%negnegint(X)
-%negnegel(X)
+%negnegint
+%negnegel
 verifyLine([LineNum, Term, negnegint(X)], _, Proof) :- X < LineNum, findTerm(X, Proof, TermX), arg(1, Term, InTerm), arg(1, InTerm, TermX), compareLevel(LineNum, Proof, negnegint(X,Y)), compareBoxNumber(LineNum, Proof, negnegint(X)), lineVerifyPrint(LineNum). 
 verifyLine([LineNum, Term, negnegel(X)], _, Proof) :- X < LineNum, findTerm(X, Proof, neg(neg(Term))), compareLevel(LineNum, Proof, negnegel(X,Y)), compareBoxNumber(LineNum, Proof, negnegel(X)), lineVerifyPrint(LineNum).
 
@@ -63,7 +65,7 @@ verifyLine([LineNum, Term, negnegel(X)], _, Proof) :- X < LineNum, findTerm(X, P
 verifyLine([LineNum, Term, mt(X,Y)], _, Proof) :- X < LineNum , Y < LineNum, arg(1, Term, InTerm), findTerm(X, Proof, TermX), arg(1, TermX, InTerm), arg(2, TermX, InTermX ), findTerm(Y, Proof, neg(InTermX)), compareLevel(LineNum, Proof, mt(X,Y)), compareBoxNumber(LineNum, Proof, impel(X,Y)), lineVerifyPrint(LineNum).
 
 %pbc(X,Y)
-verifyLine([LineNum, Term, pbc(X,Y)], _, Proof) :- X < LineNum, Y < LineNum, findTerm(X, Proof, neg(Term)), findTerm(Y, Proof, cont), compareLevel(LineNum, Proof, pbc(X,Y)), compareBoxes(LineNum, Proof, impel(X,Y)), lineVerifyPrint(LineNum).
+verifyLine([LineNum, Term, pbc(X,Y)], _, Proof) :- X < LineNum, Y < LineNum, findTerm(X, Proof, neg(Term)), findTerm(Y, Proof, cont), compareLevel(LineNum, Proof, pbc(X,Y)), lineVerifyPrint(LineNum).
 
 %lem
 verifyLine([LineNum, or(neg(P), P), lem], _, Proof) :- lineVerifyPrint(LineNum).
@@ -81,13 +83,8 @@ findTerm(LineNum, [[Line|Ts]|T], Term) :- findTerm(LineNum, Ts, Term).
 findTerm(LineNum, [Line|T], Term) :- findTerm(LineNum, T, Term).
 
 lineVerifyPrint(LineNum) :- write("Line "),write(LineNum),write(" fullfilled"),write("\n").
-levelPrint(LineNum) :- write("Box number "),write(LineNum),write("\n").
+%testPrint(LineNum) :- write("Box number "),write(LineNum),write("\n").
 
-show_records([]).
-show_records([A|B]) :-
-  write(A),
-  write('\t'),
-  show_records(B).
 
 %Finds the level of a line
 boxLevel(LineNum,[], BoxLevel, ResBoxLevel) :- false.
@@ -95,6 +92,7 @@ boxLevel(LineNum, [Line|T], BoxLevel, ResBoxLevel) :- getFirstOfList(Line, LineN
 boxLevel(LineNum, [Line|T], BoxLevel, ResBoxLevel) :- getFirstOfList(Line, H), isList(H), NewLevel is BoxLevel + 1, boxLevel(LineNum, Line, NewLevel, ResBoxLevel).
 boxLevel(LineNum, [Line|T], BoxLevel, ResBoxLevel) :- boxLevel(LineNum, T, BoxLevel, ResBoxLevel).
 
+%Counts how many boxes you have and the starting line for each box
 countBoxes([], Count, ResCount, StartNum, ResStartNum) :- ResCount = Count, ResStartNum = StartNum.
 countBoxes([Line|T], Count, ResCount, StartNum, [LineNum|ResStartNum]) :- getFirstOfList(Line, H), isList(H), getFirstOfList(H, LineNum), NewCount is Count + 1, countBoxes(T, NewCount, ResCount, StartNum , ResStartNum).
 countBoxes([Line|T], Count, ResCount, StartNum, ResStartNum) :- countBoxes(T, Count, ResCount, StartNum, ResStartNum).
@@ -106,24 +104,34 @@ detBox(LineNum, Proof, ResBoxNum) :- countBoxes(Proof, 0, Boxes, [] , StartNum),
 
 elementComp([], Element, Pos, ResPos) :- ResPos is 0.
 elementComp([H|[]], Element, Pos, ResPos) :- ResPos is Pos + 1.
-elementComp([H|T], Element, Pos, ResPos) :- Element > H, getFirstOfList(T, Ht), Element < Ht, ResPos is Pos + 1.
+elementComp([H|T], Element, Pos, ResPos) :- Element >= H, getFirstOfList(T, Ht), Element < Ht, ResPos is Pos + 1.
 elementComp([H|T], Element, Pos, ResPos) :- NewPos is Pos + 1, elementComp(T, Element, NewPos, ResPos).
 
 %Checks if argument is list 
 isList([_|_]).
 isList([]).
 
-compareBoxNumber(LineNum, Proof, Func) :- arg(1, Func, Value), not(arg(2, Func, _)), detBox(Value, Proof, ResValue), detBox(LineNum, Proof, ResLineNum), !, ResLineNum = ResValue.
+%Makes sure that all Func point to the same box or one of the two values points to something outside any box.
 compareBoxNumber(LineNum, Proof, Func) :- arg(1, Func, Value), not(arg(2, Func, _)), detBox(Value, Proof, ResValue), detBox(LineNum, Proof, ResLineNum), ResValue = 0.
-compareBoxNumber(LineNum, Proof, Func) :- arg(1, Func, Value), arg(2, Func, SecValue), detBox(Value, Proof, ResValue), detBox(SecValue, Proof, ResSecValue), detBox(LineNum, Proof, ResLineNum), !, ResValue = ResSecValue.
-%compareBoxNumber(LineNum, Proof, Func) :- arg(1, Func, Value), arg(2, Func, SecValue), detBox(Value, Proof, ResValue),  detBox(SecValue, Proof, ResSecValue), detBox(LineNum, Proof, ResLineNum),levelPrint(ResLineNum), ResValue == ResLineNum, ResSecValue == ResLineNum.
+compareBoxNumber(LineNum, Proof, Func) :- arg(1, Func, Value), not(arg(2, Func, _)), detBox(Value, Proof, ResValue), detBox(LineNum, Proof, ResLineNum), !, ResLineNum = ResValue.
+compareBoxNumber(LineNum, Proof, Func) :- arg(1, Func, Value), arg(2, Func, SecValue), detBox(Value, Proof, ResValue), detBox(SecValue, Proof, ResSecValue), !, boxNumberHelper(ResValue, ResSecValue).
+compareBoxNumber(LineNum, Proof, Func) :- arg(1, Func, Value), arg(2, Func, Value2), arg(3, Func, Value3), arg(4, Func, Value4), arg(5, Func, Value5), 
+detBox(Value, Proof, ResValue), detBox(Value2, Proof, ResValue2), detBox(Value3, Proof, ResValue3), detBox(Value4, Proof, ResValue4), detBox(Value5, Proof, ResValue5), detBox(LineNum, Proof, ResLineNum),
+boxNumberHelper(ResValue, ResValue2, ResValue3, ResValue4, ResValue5, ResLineNum).
 
-%Comapres which box you refer to.
-%compareBoxes(LineNum, Proof, Func) :- not(arg(2, Func, Value)).
-%compareBoxes(LineNum, Proof, Func) :- arg(1, Func, Value), arg(2, Func, SecValue), detBox(Value, Proof, ResValue), detBox(SecValue, Proof, ResSecValue),  levelPrint(ResValue), levelPrint(ResSecValue), !, ResValue = ResSecValue.
+boxNumberHelper(ResValue, ResValue2, ResValue3, ResValue4, ResValue5, ResLineNum) :- ResValue2 = ResValue3, ResValue4 = ResValue5, ResLineNum = ResValue.
+boxNumberHelper(0, ResValue2, ResValue3, ResValue4, ResValue5, ResLineNum) :- ResValue2 = ResValue3, ResValue4 = ResValue5.
+boxNumberHelper(ResValue, 0) :- true.
+boxNumberHelper(ResValue, ResSecValue) :- ResValue = ResSecValue.
+boxNumberHelper(0, ResSecValue) :- true.
 
-%Comapres the level of different lines. This makes sure that they are at most within 1 level of eachother
+%Comapres the level of different lines. This makes sure that the targets of the Func are at most within 1 level of eachother. And for those that should be on the same level, it checks for that too.
 compareLevel(LineNum, Proof, Func) :- arg(1, Func, Value), boxLevel(LineNum, Proof, 0, Level), boxLevel(Value, Proof, 0, ValueLevel), Level >= ValueLevel.
 compareLevel(LineNum, Proof, Func) :- arg(1, Func, Value), arg(2, Func, SecValue), 
 boxLevel(LineNum, Proof, 0, Level), boxLevel(Value, Proof, 0, ValueLevel), boxLevel(SecValue, Proof, 0, SecValueLevel), Diff is ValueLevel-Level, Diff2 is SecValueLevel-Level,
 Diff < 2, Diff2 < 2, ValueLevel == SecValueLevel.
+compareLevel(LineNum, Proof, Func) :- arg(1, Func, Value), arg(2, Func, Value2), arg(3, Func, Value3), arg(4, Func, Value4), arg(5, Func, Value5),
+boxLevel(LineNum, Proof, 0, Level), boxLevel(Value, Proof, 0, ValueLevel), boxLevel(Value2, Proof, 0, ValueLevel2), boxLevel(Value3, Proof, 0, ValueLevel3),
+boxLevel(Value4, Proof, 0, ValueLevel4), boxLevel(Value5, Proof, 0, ValueLevel5),
+ValueLevel2 = ValueLevel3, ValueLevel4 = ValueLevel5,
+Diff is ValueLevel-Level, Diff < 2.
